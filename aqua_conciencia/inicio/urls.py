69edf5c968d8140b60a4710e230f6f7f9671ba10
Maykol Coronado Risco <maykolrafael.coronado@alumnos.ulagos.cl>
2026-06-13 17:28:43 -0400

    Se agregan archivos css como "error 404" entre otros

--- aqua_conciencia/inicio/urls.py
+++ aqua_conciencia/inicio/urls.py
@@ -4,3 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.inicio,
     name='inicio'),]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('restablecer/', views.restablecer, name='restablecer'),
+]
+
+handler404 = 'inicio.views.error_404'
--- aqua_conciencia/inicio/views.py
+++ aqua_conciencia/inicio/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def inicio(request):
-    return HttpResponse("¡Bienvenido a AquaConciencia!") 
+    return render(request, 'inicio/login.html')
+
+def restablecer(request):
+    return render(request, 'inicio/restablecer_contraseña.html')
+
+def error_404(request, exception):
+    return render(request, 'inicio/error_404.html', status=404)
