c87ad26681be508b05f27a99fc1a8bc0286e5959
Víctor Saldivia Millalonco <saldiviavictor1925@gmail.com>
2026-06-13 21:06:43 -0400

    Login y navegacion funcionando

--- aqua_conciencia/inicio/urls.py
+++ aqua_conciencia/inicio/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.inicio,
-    name='inicio'),]
-
-from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('crear-cuenta/', views.crear_cuenta, name='crear_cuenta'),
     path('restablecer/', views.restablecer, name='restablecer'),
 ]
 
--- aqua_conciencia/inicio/views.py
+++ aqua_conciencia/inicio/views.py
@@ -5,6 +5,7 @@ def inicio(request):
 
 def restablecer(request):
     return render(request, 'inicio/restablecer_contraseña.html')
-
+def crear_cuenta(request):
+    return render(request, 'inicio/creacion_cuenta.html')
 def error_404(request, exception):
     return render(request, 'inicio/error_404.html', status=404)
